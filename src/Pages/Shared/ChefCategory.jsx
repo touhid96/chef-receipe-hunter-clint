1e472fa2b7e78d4fe31e89e2eefbb36a2b202d07
touhid <touhid.islam96@gmail.com>
2024-03-08 02:36:30 +0100

    new commit

--- src/Pages/Shared/ChefCategory.jsx
+++ src/Pages/Shared/ChefCategory.jsx
@@ -1,42 +1,42 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from 'react';
-import ChefCategoryDetails from '../../layout/ChefCategoryDetails';
+import React, { useEffect, useState } from "react";
+import ChefCategoryDetails from "../../layout/ChefCategoryDetails";
 
 const ChefCategory = () => {
-    const [chefdata, setChefdata] = useState([])
-    const [showAll, setShowAll] = useState(false)
-    useEffect(() => {
-        fetch('https://chef-recipe-hunter-server-hasan-fahad.vercel.app/chefdata')
-        .then(res => res.json())
-        .then(data => setChefdata(data))
-        .catch(error => console.error(error))
-    },[])
-    return (
-        <div className='mt-8'>
-            <h1 className='text-3xl font-bold text-center'>Our Main Chef Is Here</h1>
-            <div >
-                <div className='grid grid-cols-1 sm:grid-cols-2 gap-5 mt-8'>
-                    { showAll ? 
-                        chefdata.map(chef => <ChefCategoryDetails key={chef.chef_id} chef={chef}></ChefCategoryDetails>)
-                        : chefdata
-                        .slice(0, 4)
-                        .map(chef => <ChefCategoryDetails key={chef.chef_id} chef={chef}></ChefCategoryDetails>)
-                    }
-                </div>
-                <div className="text-center">
-                        {!showAll && (
-                        <button
-                            onClick={() => setShowAll(true)}
-                            className="btn btn-sm btn-secondary my-4"
-                          >
-                            Show All
-                        </button>
-                )}
-                </div>
-            </div>
+  const [chefdata, setChefdata] = useState([]);
+  const [showAll, setShowAll] = useState(false);
+  useEffect(() => {
+    fetch("https://chef-receipe-hunter-server-69grac9y9.vercel.app/chefdata")
+      .then((res) => res.json())
+      .then((data) => setChefdata(data))
+      .catch((error) => console.error(error));
+  }, []);
+  return (
+    <div className="mt-8">
+      <h1 className="text-3xl font-bold text-center">Our Main Chef Is Here</h1>
+      <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 mt-8">
+          {showAll
+            ? chefdata.map((chef) => (
+                <ChefCategoryDetails key={chef.chef_id} chef={chef}></ChefCategoryDetails>
+              ))
+            : chefdata
+                .slice(0, 4)
+                .map((chef) => (
+                  <ChefCategoryDetails key={chef.chef_id} chef={chef}></ChefCategoryDetails>
+                ))}
         </div>
-    );
+        <div className="text-center">
+          {!showAll && (
+            <button onClick={() => setShowAll(true)} className="btn btn-sm btn-secondary my-4">
+              Show All
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChefCategory;
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,71 +1,76 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from 'react';
-import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
-import app from '../firebase/firebase.config';
-import { createContext } from 'react';
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
-export const auth = getAuth(app)
-const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null)
-    const [loading, setLoading] = useState(true)
-    const provider = new GoogleAuthProvider()
-    const githubProvider = new GithubAuthProvider();
-    const createUser = (email, password)=>{
-        setLoading(true)
-        return createUserWithEmailAndPassword(auth, email, password)
-    }
-    const signIn = (email, password) =>{
-        setLoading(true)
-        return signInWithEmailAndPassword(auth, email, password)
-    }
-    const updateUser = (name, photo) =>{
-        setLoading(true)
-        return updateProfile(name, photo)
-    }
-    const logOut = () =>{
-        setLoading(true)
-        return signOut(auth);
-    }
+export const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const provider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const updateUser = (name, photo) => {
+    setLoading(true);
+    return updateProfile(name, photo);
+  };
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
 
-    useEffect(()=>{
-      const unsubscribe = onAuthStateChanged(auth, loggedUser => {
-            setUser(loggedUser)
-            setLoading(false)
-        })
-        return () =>{ 
-            unsubscribe()
-        }
-    },[])
-    const updateUserData = (user, name, photoURL)=>{
-        updateProfile(user,{
-          displayName:name, photoURL: photoURL
-        })
-        .then(()=>{
-          console.log('user name and photo URL updated');
-        })
-        .catch(error =>{
-          console.log(error)
-        })
-      }
-    const authInfo ={
-        user,
-        createUser,
-        signIn,
-        updateProfile,
-        logOut,
-        loading,
-        provider,
-        githubProvider,
-        updateUserData
-    }
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (loggedUser) => {
+      setUser(loggedUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  const updateUserData = (user, name, photoURL) => {
+    updateProfile(user, {
+      displayName: name,
+      photoURL: photoURL,
+    })
+      .then(() => {
+        console.log("user name and photo URL updated");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const authInfo = {
+    user,
+    createUser,
+    signIn,
+    updateProfile,
+    logOut,
+    loading,
+    provider,
+    githubProvider,
+    updateUserData,
+  };
+  return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
